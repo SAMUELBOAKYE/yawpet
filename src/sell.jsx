3a29d54e170964c10bff963d67879de825a15c26
Boakye Samuel <boakyesamuel189@gmail.com>
2025-06-18 22:50:24 -0700

    Fix: case-sensitive import for sellForm.css

--- src/sell.jsx
+++ src/sell.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./SellForm.css";
+import "./sellForm.css";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
